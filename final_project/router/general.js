18b59c289435d745283926b7418f497ded792103
Liang Zhang <liang.lewis.z@gmail.com>
2024-07-10 05:22:05 -0400

    completed look by title, look for review and user registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.query;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    const userExists = users.some(user => user.username === username);
+
+    if (userExists) {
+        return res.status(409).json({ message: "Username already exists" });
+    }
+
+    users.push({ username, password });
+    return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
@@ -50,14 +62,35 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const keys = Object.keys(books);
+  const result = [];
+
+  keys.forEach(key => {
+    if (books[key].title.toLowerCase() === title.toLocaleLowerCase()) {
+        result.push(books[key])
+    }
+  });
+
+  if (result.length > 0) {
+    return res.status(200).send(JSON.stringify(result, null, 2));
+  } else {
+    return res.status(404).json({message: "No book found for this title"});
+  }
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    // Find the review with the isbn
+    let review = book.review;
+    return res.send(JSON.stringify(review, null, 2));
+  }
+
+  return res.send("Book with the isbn provided not found");
 });
 
 module.exports.general = public_users;
