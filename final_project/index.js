a00050628c0ceca04ff6992c3c4cb92a65f7e246
Liang Zhang <liang.lewis.z@gmail.com>
2024-07-10 04:27:39 -0400

    completed get details for isbn and get books by authorfeature

--- final_project/index.js
+++ final_project/index.js
@@ -29,7 +29,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
     }
 });
  
-const PORT =5001;
+const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,10 +18,10 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
    const isbn = req.params.isbn;
-   const book = books.find(b => b.isbn === isbn);
+   const book = books[isbn];
 
     if (book) {
-        return res.status(200).send(JSON.stringify(book, null, 2)); // Neatly formatted JSON output
+        return res.status(200).send(JSON.stringify(book, null, 4)); // Neatly formatted JSON output
     } else {
         return res.status(404).json({ message: "Book not found" });
     }
@@ -30,8 +30,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+    const results = [];
+
+    keys.forEach(key => {
+        if (books[key].author.toLowerCase() === author.toLowerCase()) {
+            results.push(books[key]);
+        }
+    });
+
+    if (results.length > 0) {
+        return res.status(200).send(JSON.stringify(results, null, 2)); // Neatly formatted JSON output
+    } else {
+        return res.status(404).json({ message: "No books found by this author" });
+    }
+  
 });
 
 // Get all books based on title
