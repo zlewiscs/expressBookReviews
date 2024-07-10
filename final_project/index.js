dd080a2cab0d4ae2fdb65f07cb7e593804b53006
Liang Zhang <liang.lewis.z@gmail.com>
2024-07-10 01:11:19 -0400

    Implemented user authentication.

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,19 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+const token = req.headers["authorization"];
+
+    if (!token) {
+        return res.status(401).json({ message: "Access token is missing" });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Invalid access token" });
+        }
+        req.user = decoded; // You can store decoded information (e.g., user details) in req.user
+        next();
+    });
 });
  
 const PORT =5000;
