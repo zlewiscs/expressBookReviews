01a4860c7a8065452ee5a4ad74408667b97daf52
Liang Zhang <liang.lewis.z@gmail.com>
2024-07-10 01:25:12 -0400

    Implemented features getting list of books available

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,15 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   const isbn = req.params.isbn;
+   const book = books.find(b => b.isbn === isbn);
+
+    if (book) {
+        return res.status(200).send(JSON.stringify(book, null, 2)); // Neatly formatted JSON output
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
  });
   
 // Get book details based on author
